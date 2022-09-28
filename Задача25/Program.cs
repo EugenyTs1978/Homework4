67a2e1eed0c6edf71d6fcfe67da66a0d7ef5bffc
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-09-28 17:47:59 +0500

    Домашнее задание4

---
+++ Задача25/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+ */
+int getPower(int basis, int power)
+{
+    int result = 1;
+    for(int i = 1; i <= power; i++)
+    {
+        result *= basis;
+    }
+    return result;
+}
+Console.WriteLine("Введите целое число A и натуральное число B");
+int userBasis = Convert.ToInt32(Console.ReadLine());
+int userPower = Convert.ToInt32(Console.ReadLine());
+int isPower = getPower(userBasis,userPower);
+Console.WriteLine($"{userBasis} ^ {userPower} = {isPower}");
